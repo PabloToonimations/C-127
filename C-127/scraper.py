2755612d54e74a3842548505af67eefc6db1f269
PabloToonimations <79716578+PabloToonimations@users.noreply.github.com>
2024-03-04 17:50:13 -0600

    Add files via upload

---
+++ C-127/scraper.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup
+import time
+import pandas as pd
+
+# Enlace a NASA Exoplanet
+START_URL = "https://exoplanets.nasa.gov/exoplanet-catalog/"
